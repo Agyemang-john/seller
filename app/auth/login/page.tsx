f4b32b9c3a265cc182c12f024f97fa5a259c5475
John Agyemang <oseiagyemanjohn@gmail.com>
2025-10-22 13:14:58 +0000

    Login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { Metadata } from "next";
 import { Suspense } from 'react';
 import LoginForm from "@/components/forms/LoginForm";
-import { generateMetadata } from "@/utils/metadata";
 import useRedirectIfAuthenticated from '@/hooks/useRedirectIfAuthenticated';
 
-export const metadata: Metadata = generateMetadata("Login", "");
 
 const Login = () => {
   useRedirectIfAuthenticated();
