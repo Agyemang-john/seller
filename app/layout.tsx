4d8fdd3a9c4b427010096ce2d317d614c64d37b5
John Agyemang <oseiagyemanjohn@gmail.com>
2025-11-03 23:47:15 +0000

    WEBMASTER

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
         <meta name="msvalidate.01" content="5DA71870689BE44E8BBAC87A7BF7461A" />
         
         {/* Bing */}
-        <meta name="msvalidate.01" content="347ABCC9900620D6D0780EB419BA2B57" /> 
+        <meta name="msvalidate.01" content="C0B1BE8D62E65099CC0EC26BC04B076E" />
         <link rel="icon" href="/favicon-new.ico" sizes="any" />
       </head>
       <body
